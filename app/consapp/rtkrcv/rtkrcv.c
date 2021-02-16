b1599503d19358fec4aa2f082b7c25e8b8aed076
Tim Schryvers <tschryvers@positionpartners.com.au>
2021-02-16 13:43:29 +1100

    comenting out confwrite finction in startsvr as it segfaults

--- app/consapp/rtkrcv/rtkrcv.c
+++ app/consapp/rtkrcv/rtkrcv.c
@@ -420,9 +420,11 @@ static int startsvr(vt_t *vt)
         else cmds_periodic[i]=s2[i];
     }
     /* confirm overwrite */
+    /* Segfault when ran. commenting out for docker container build */
+    /*
     for (i=3;i<8;i++) {
         if (strtype[i]==STR_FILE&&!confwrite(vt,strpath[i])) return 0;
-    }
+    }*/
     if (prcopt.refpos==4) { /* rtcm */
         for (i=0;i<3;i++) prcopt.rb[i]=0.0;
     }
